9cb053bbe318fa59c6f98e6063160a775709b807
Maurice <mauriceboendermaker@gmail.com>
2026-03-25 22:57:31 +0100

    v4.0

--- src/Game.java
+++ src/Game.java
@@ -323,7 +323,7 @@ public class Game extends Canvas implements Runnable {
         Settings.getMusicVolume(); // Force settings load & volume sync
         AudioPlayer.play();
 
-        new Window(windowWidth, windowHeight, "Dotch. - v3.0", this);
+        new Window(windowWidth, windowHeight, "Dotch. - v4.0", this);
 
         spawner = new Spawn(handler, hud, this);
         spawnerRef = spawner;
